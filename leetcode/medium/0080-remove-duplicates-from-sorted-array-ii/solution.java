996d15288ad06c3751d65ffe5a0d3d75b182a1eb
Tejas Ingale <142830835+Ingale-Tejas@users.noreply.github.com>
2026-08-11 19:10:58 +0530

    ✅ Remove Duplicates from Sorted Array II | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 48.9 MB | 2026-08-11

--- leetcode/medium/0080-remove-duplicates-from-sorted-array-ii/solution.java
+++ leetcode/medium/0080-remove-duplicates-from-sorted-array-ii/solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
 
-     int officer=2;
+     int officer=1;
     int cm = 2;
     int res =2;
     int n = nums.length;
 
         while(cm < n) {
             
-            if(nums[cm] == nums[officer-2]){
+            if(nums[cm] == nums[officer-1]){
                 cm++;
                 continue;
             }
-            nums[officer] = nums[cm];
+            nums[officer + 1] = nums[cm];
             officer++;
             res++;
             cm++;
